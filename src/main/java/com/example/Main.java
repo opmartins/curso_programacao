2ef62213aba336565d088070540ec4a1a835640b
opmartins <opmartins@outlook.com>
2025-03-23 14:37:49 -0300

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int  y = 32;
+        double x = 10.32131;
+        System.out.printf("%.2f%n",x);
+        System.out.println("Hello world!");
+    }
+}
